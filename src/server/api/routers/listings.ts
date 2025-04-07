e5cf4d0dc1e4e1e4d3d509a74a5164ad9f814595
shawntan0301 <shawntan0301@gmail.com>
2025-04-07 12:56:23 +0800

    added enums to schema and chaned listings routes

--- src/server/api/routers/listings.ts
+++ src/server/api/routers/listings.ts
@@ -1,139 +1,164 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import {
-    createTRPCRouter,
-    publicProcedure,
-    protectedProcedure,
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
 } from "~/server/api/trpc";
+import { Category, Condition, DealMethod } from "@prisma/client";
 
 export const listingsRouter = createTRPCRouter({
+  createListing: protectedProcedure
+    .input(
+      z.object({
+        title: z.string().min(1),
+        description: z.string().min(1),
+        price: z.number().nonnegative(),
+        imageUrl: z.string().url(),
+        brand: z.string().optional(),
+        category: z.nativeEnum(Category),
+        condition: z.nativeEnum(Condition),
+        dealMethods: z.array(z.nativeEnum(DealMethod)),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.listing.create({
+        data: {
+          ...input,
+          userId: ctx.session.userId,
+        },
+      });
+    }),
 
-    // Create a listing (must be logged in)
-    createListing: protectedProcedure
-        .input(
-            z.object({
-                title: z.string().min(1),
-                description: z.string().min(1),
-                price: z.number().nonnegative(),
-                imageUrl: z.string().url(),
-                category: z.string(),
-            })
-        )
-        .mutation(async ({ ctx, input }) => {
-            return await ctx.db.listing.create({
-                data: {
-                    ...input,
-                    userId: ctx.session.userId,
-                },
-            });
-        }),
+  editListing: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        title: z.string().optional(),
+        description: z.string().optional(),
+        price: z.number().optional(),
+        imageUrl: z.string().url().optional(),
+        brand: z.string().optional(),
+        category: z.nativeEnum(Category).optional(),
+        condition: z.nativeEnum(Condition).optional(),
+        dealMethods: z.array(z.nativeEnum(DealMethod)).optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const listing = await ctx.db.listing.findUnique({
+        where: { id: input.id },
+      });
 
-    editListing: protectedProcedure
-        .input(
-            z.object({
-                id: z.string(),
-                title: z.string().optional(),
-                description: z.string().optional(),
-                price: z.number().optional(),
-                imageUrl: z.string().optional(),
-            }),
-        )
-        .mutation(async ({ ctx, input }) => {
-            const user = await ctx.db.user.findUnique({ where: { id: input.id } });
-            const listing = await ctx.db.listing.findUnique({
-                where: { id: input.id },
-            });
+      if (!listing || listing.userId !== ctx.session.userId) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Listing not found or not owned by user",
+        });
+      }
 
-            if (!listing) throw new TRPCError({ code: "NOT_FOUND", message: "Listing not found" });
-            if (listing.userId !== user?.id) throw new TRPCError({ code: "NOT_FOUND", message: "Listing not found" });
+      return await ctx.db.listing.update({
+        where: { id: input.id },
+        data: {
+          title: input.title,
+          description: input.description,
+          price: input.price,
+          imageUrl: input.imageUrl,
+          brand: input.brand,
+          category: input.category,
+          condition: input.condition,
+          dealMethods: input.dealMethods,
+        },
+      });
+    }),
 
-            return await ctx.db.listing.update({
-                where: { id: input.id },
-                data: {
-                    title: input.title,
-                    description: input.description,
-                    price: input.price,
-                    imageUrl: input.imageUrl,
-                },
-            });
-        }),
+  // Delete a listing
+  deleteListing: protectedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      const listing = await ctx.db.listing.findUnique({ where: { id: input } });
 
-    // Delete a listing
-    deleteListing: protectedProcedure
-        .input(z.string())
-        .mutation(async ({ ctx, input }) => {
-            const listing = await ctx.db.listing.findUnique({ where: { id: input } });
+      if (!listing)
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Listing not found",
+        });
+      if (listing.userId !== ctx.session.userId)
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Listing not found",
+        });
 
-            if (!listing) throw new TRPCError({ code: "NOT_FOUND", message: "Listing not found" });
-            if (listing.userId !== ctx.session.userId) throw new TRPCError({ code: "NOT_FOUND", message: "Listing not found" });
+      return await ctx.db.listing.delete({ where: { id: input } });
+    }),
 
-            return await ctx.db.listing.delete({ where: { id: input } });
-        }),
+  // Get a single listing by ID
+  getSingleListing: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const listing = await ctx.db.listing.findUnique({
+        where: { id: input.id },
+        include: { user: true },
+      });
 
-    // Get a single listing by ID 
-    getSingleListing: publicProcedure
-        .input(z.object({ id: z.string() }))
-        .query(async ({ ctx, input }) => {
-            const listing = await ctx.db.listing.findUnique({
-                where: { id: input.id },
-                include: { user: true },
-            });
+      if (!listing) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Listing not found",
+        });
+      }
 
-            if (!listing) {
-                throw new TRPCError({ code: "NOT_FOUND", message: "Listing not found" });
-            }
+      return listing;
+    }),
 
-            return listing;
-        }),
+  // Get all listings
+  getAllListings: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.db.listing.findMany({
+      orderBy: { createdAt: "desc" },
+      include: { user: true },
+    });
+  }),
 
-    // Get all listings
-    getAllListings: publicProcedure
-        .query(async ({ ctx }) => {
-            return await ctx.db.listing.findMany({
-                orderBy: { createdAt: "desc" },
-                include: { user: true },
-            });
-        }),
+  // Get all listings by the current user
+  getAllListingsById: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.listing.findMany({
+      where: { userId: ctx.session.userId },
+    });
+  }),
 
-    // Get all listings by the current user
-    getAllListingsById: protectedProcedure
-        .query(async ({ ctx }) => {
-            return await ctx.db.listing.findMany({
-                where: { userId: ctx.session.userId },
-            });
-        }),
+  // Search listings by keyword (can consider adding search everytime a letter is written)
+  searchListings: publicProcedure
+    .input(z.object({ query: z.string().min(1) }))
+    .query(async ({ ctx, input }) => {
+      const results = await ctx.db.listing.findMany({
+        where: {
+          OR: [
+            { title: { contains: input.query, mode: "insensitive" } },
+            { description: { contains: input.query, mode: "insensitive" } },
+          ],
+        },
+        orderBy: { createdAt: "desc" },
+        include: { user: true },
+      });
 
-    // Search listings by keyword (can consider adding search everytime a letter is written)
-    searchListings: publicProcedure
-        .input(z.object({ query: z.string().min(1) }))
-        .query(async ({ ctx, input }) => {
-            const results = await ctx.db.listing.findMany({
-                where: {
-                    OR: [
-                        { title: { contains: input.query, mode: "insensitive" } },
-                        { description: { contains: input.query, mode: "insensitive" } },
-                    ],
-                },
-                orderBy: { createdAt: "desc" },
-                include: { user: true },
-            });
+      return results;
+    }),
 
-            return results;
-        }),
+  // Filter listings by category
+  filterListingsByCategory: publicProcedure
+    .input(
+      z.object({
+        category: z.nativeEnum(Category),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const listings = await ctx.db.listing.findMany({
+        where: {
+          category: input.category,
+        },
+        orderBy: { createdAt: "desc" },
+        include: { user: true },
+      });
 
-    // Filter listings by category (check)
-    filterListingsByCategory: publicProcedure
-        .input(z.object({ category: z.string() }))
-        .query(async ({ ctx, input }) => {
-            const listings = await ctx.db.listing.findMany({
-                where: {
-                    category: input.category,
-                },
-                orderBy: { createdAt: "desc" },
-                include: { user: true },
-            });
-
-            return listings;
-        }),
-
+      return listings;
+    }),
 });
